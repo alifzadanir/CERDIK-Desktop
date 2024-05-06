8ad218574d4d54fd7e380e2e7129f138716d1b44
alifzadanir <alifadanir@gmail.com>
2024-05-06 15:10:03 +0700

    Menambahkan Library Menu

--- CERDIK/Program.cs
+++ CERDIK/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Generic
+
+using System.ComponentModel.Design;
+using CERDIK;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<DevicesAddConfig> devices = new List<DevicesAddConfig>();
+        string pilihMenu;
+        string pilihRole;
+        Menu.Menus.menuUtama();
+
+        Console.WriteLine("Pilih role sesuai dengan kebutuhanmu!");
+        pilihRole = Console.ReadLine();
+        if (pilihRole == "1" || pilihRole == "2")
+        {
+            Menu.Menus.menuLogin();
+        }
+
+        Console.WriteLine("Pilih:");
+        pilihMenu = Console.ReadLine();
+        if (pilihMenu == "1" && pilihRole == "1")
+        {
+            Console.WriteLine("Login Sukses!");
+            Menu.Menus.menuPasien();
+        }
+        else if (pilihMenu == "2" && pilihRole == "1")
+        {
+            Console.WriteLine("Daftar Sukses!");
+            Menu.Menus.menuPasien();
+        }
+        else if (pilihMenu == "1" && pilihRole == "2")
+        {
+            Console.WriteLine("Login Sukses!");
+            Menu.Menus.menuNakes();
+        }
+        else if (pilihMenu == "2" && pilihRole == "2")
+        {
+            Console.WriteLine("Daftar Sukses!");
+            Menu.Menus.menuNakes();
+        }
+        Console.WriteLine("Pilihan:");
+        pilihMenu = Console.ReadLine();
+
+    }
+}
