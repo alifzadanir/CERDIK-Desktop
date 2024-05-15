6267cf3dd513e41a94447e17b33b45818c6aba79
arfannabiell <162048018+arfannabiell@users.noreply.github.com>
2024-05-15 21:59:43 +0700

    menambahkan API

---
+++ API_CERDIK/Controllers/JadwalController.cs
@@ -0,0 +1,90 @@
+﻿using API_CERDIK.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API_CERDIK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class JadwalController : Controller
+    {
+        private static List<Jadwal> DataJadwal = new List<Jadwal>();
+
+        public JadwalController()
+        {
+            string jsonFilePath = "C:\\Users\\Arfan\\OneDrive\\Documents\\Tugas Kuliah\\KPL\\CERDIK-Desktop\\API_CERDIK\\Data\\DataJadwal.json";
+            string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+            DataJadwal = JsonConvert.DeserializeObject<List<Jadwal>>(jsonData);
+        }
+        public static List<Jadwal> getDataJadwal()
+        {
+            return DataJadwal;
+        }
+
+        [HttpGet]
+        public IEnumerable<Jadwal> GET()
+        {
+            return DataJadwal;
+        }
+
+        // GET DATA BY Nama
+        [HttpGet("{NamaPasien}")]
+        public Jadwal GETAnggota(string NamaPasien)
+        {
+            int id = -1;
+
+            for (int i = 0; i < DataJadwal.Count; i++)
+            {
+                if (NamaPasien == DataJadwal[i].pasien)
+                {
+                    id = i;
+                }
+            }
+            return DataJadwal[id];
+        }
+
+        // POST DATA
+        [HttpPost]
+        public void POST([FromBody] Jadwal newJadwal)
+        {
+            int id = -1;
+            Boolean sama = false;
+
+            for (int i = 0; i < DataJadwal.Count; i++)
+            {
+                if (newJadwal.pasien == DataJadwal[i].pasien)
+                {
+                    sama = true;
+                }
+            }
+
+            if (!sama)
+            {
+                DataJadwal.Add(newJadwal);
+                string jsonFilePath = "C:\\Users\\Arfan\\OneDrive\\Documents\\Tugas Kuliah\\KPL\\CERDIK-Desktop\\API_CERDIK\\Data\\DataJadwal.json";
+                string jsonContent = JsonConvert.SerializeObject(DataJadwal);
+                System.IO.File.WriteAllText(jsonFilePath, jsonContent);
+            }
+        }
+
+        // DELETE DATA 
+        [HttpDelete("{Nama_Pasien}")]
+        public void DELETE(String Nama_Pasien)
+        {
+            int id = -1;
+
+            for (int i = 0; i < DataJadwal.Count; i++)
+            {
+                if (Nama_Pasien == DataJadwal[i].pasien)
+                {
+                    DataJadwal.RemoveAt(i);
+                    string jsonFilePath = "C:\\Users\\Arfan\\OneDrive\\Documents\\Tugas Kuliah\\KPL\\CERDIK-Desktop\\API_CERDIK\\Data\\DataJadwal.json";
+                    string jsonContent = JsonConvert.SerializeObject(DataJadwal);
+                    System.IO.File.WriteAllText(jsonFilePath, jsonContent);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ API_CERDIK/Model/Jadwal.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace API_CERDIK.Model
+{
+    public class Jadwal
+    {
+        public string tanggal { get; set; }
+
+        public string waktu_mulai { get; set; }
+
+        public string waktu_selesai { get; set; }
+
+        public string dokter { get; set; }
+
+        public string pasien { get; set; }
+        public string keterangan { get; set; }
+
+        public Jadwal(string tanggal, string waktu_mulai, string waktu_selesai, string dokter, string pasien, string keterangan)
+        {
+            this.tanggal = tanggal;
+            this.waktu_mulai = waktu_mulai;
+            this.waktu_selesai = waktu_selesai;
+            this.dokter = dokter;
+            this.pasien = pasien;
+            this.keterangan = keterangan;
+        }
+
+
+
+    }
+}
